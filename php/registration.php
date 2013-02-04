2c9c423a18d70c6d1d626ebabbd1bc7bf873b553
reamon26 <reamon26@mail.ru>
2013-02-05 00:08:19 +0400

    Dobavlenie kursa

--- php/registration.php
+++ php/registration.php
@@ -1,5 +1,4 @@
 <?php
-	
 	session_start();
 	include 'db.php';
     $Cname = $_POST['Cname'];
@@ -32,7 +31,7 @@
 				//$_SESSION['id'] = $row['id'];
 				//$regged = true;
 				//include ("template/registration.php"); //подключаем шаблон*/
-				createControl(strtolower($Cname_eng));
+				echo "go";
 
 		} else echo "mimo";
 	}
@@ -52,8 +51,7 @@ function translit($str)
             "м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r",
             "с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"h",
             "ц"=>"ts","ч"=>"ch","ш"=>"sh","щ"=>"sch","ъ"=>"y",
-            "ы"=>"yi","ь"=>"","э"=>"e","ю"=>"yu","я"=>"ya",
-            " "=>"_"
+            "ы"=>"yi","ь"=>"","э"=>"e","ю"=>"yu","я"=>"ya"
         );
         return strtr($str,$translit);
     }
@@ -68,32 +66,6 @@ function generateCode($length = 6)
 		   }
 		   return $string;
 		}
-function createControl($name){
-	$filename = '../application/controllers/controller_'.$name.'.php';
-	$fp = fopen($filename,'w+');
-	if(is_writable($filename)){
-		$mytext = '<?php
-					include "application/models/model_company.php";
-	            	class Controller_'.$name.' extends Controller{
-			            function __construct(){
-				            $this->model = new Model_Company();
-				            $this->view = new View();
-				        }
-				        function action_index() {
-				            $data = $this->model->get_data();
-				            $this->view->generate("company_view.php","template_view.php",$data);
-			        	}
-		            }'; // Исходная строка
-		$test = fwrite($fp, $mytext);
-		if(!$test){
-			echo "Error";
-		}
-		
-	}
-	else{
-	echo "Файл не доступен для записи";
-	}
-}
 //////////////////////////////////////
 	/*$date=time();
 	
